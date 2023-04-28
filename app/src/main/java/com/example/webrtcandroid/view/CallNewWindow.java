3047bf2ab000a1dbb413e092e9617f44e0e1e52f
Raphrodite <zhurui9427@163.com>
2023-04-28 15:48:10 +0800

    悬浮窗

---
+++ app/src/main/java/com/example/webrtcandroid/view/CallNewWindow.java
@@ -0,0 +1,168 @@
+package com.example.webrtcandroid.view;
+
+import static android.content.Context.MEDIA_PROJECTION_SERVICE;
+import static com.example.webrtcandroid.MainActivity.PROJECTION_REQUEST_CODE;
+import static com.example.webrtcandroid.webrtc.WebRtcManager.IS_SCREEN;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.webrtcandroid.CallVideoActivity;
+import com.example.webrtcandroid.IViewCallback;
+import com.example.webrtcandroid.PermissionUtil;
+import com.example.webrtcandroid.R;
+import com.example.webrtcandroid.webrtc.ProxyVideoSink;
+import com.example.webrtcandroid.webrtc.WebRtcManager;
+
+import org.webrtc.EglBase;
+import org.webrtc.MediaStream;
+import org.webrtc.RendererCommon;
+import org.webrtc.SurfaceViewRenderer;
+
+/**
+ * * Copyright * 圣通电力
+ *
+ * @ProjectName: WebrtcAndroid
+ * @Package: com.example.webrtcandroid.view
+ * @ClassName: CallNewWindow
+ * @Description: 呼叫弹窗
+ * @Author: Raphrodite
+ * @CreateDate: 2023/4/6
+ */
+public class CallNewWindow extends BaseFloatingWindow {
+
+    /**
+     * Log TAG
+     */
+    public final static String TAG = "CallNewWindow_zrzr";
+
+    private static CallNewWindow instance;
+
+    private SurfaceViewRenderer local_view;
+    private SurfaceViewRenderer remote_view;
+
+    private ProxyVideoSink localRender;
+    private ProxyVideoSink remoteRender;
+
+    private EglBase rootEglBase;
+
+    private boolean isSwappedFeeds;
+    private boolean videoEnable = true;
+
+    private WebRtcManager manager;
+
+    private static Activity mActivity;
+
+    public static CallNewWindow getInstance(Context context) {
+        if (instance == null) {
+            mActivity = (Activity) context;
+            instance = new CallNewWindow(context);
+        }
+        return instance;
+    }
+
+    public CallNewWindow(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.layout_call_new;
+    }
+
+    @Override
+    protected void initView(View mRootView) {
+        local_view = mRootView.findViewById(R.id.local_view_render);
+        remote_view = mRootView.findViewById(R.id.remote_view_render);
+
+        rootEglBase = EglBase.create();
+
+        //本地图像初始化
+        local_view.init(rootEglBase.getEglBaseContext(), null);
+        local_view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+        local_view.setZOrderMediaOverlay(true);
+        local_view.setMirror(true);
+        localRender = new ProxyVideoSink();
+        //远端图像初始化
+        remote_view.init(rootEglBase.getEglBaseContext(), null);
+        remote_view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_BALANCED);
+        remote_view.setMirror(true);
+        remoteRender = new ProxyVideoSink();
+
+        setSwappedFeeds(false);
+
+        startCall();
+    }
+
+    private void startCall() {
+        manager = WebRtcManager.getInstance();
+        manager.setCallback(new IViewCallback() {
+            @Override
+            public void onSetLocalStream(MediaStream stream) {
+                Log.e(TAG, "onSetLocalStream");
+                if (stream.videoTracks.size() > 0) {
+                    stream.videoTracks.get(0).addSink(localRender);
+                }
+
+                if (videoEnable) {
+                    stream.videoTracks.get(0).setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onSetScreenStream(MediaStream stream) {
+
+            }
+
+            @Override
+            public void onAddRemoteStream(MediaStream stream) {
+                Log.e(TAG, "onAddRemoteStream");
+                Log.e(TAG, "stream size = " + stream.videoTracks.size());
+                if (stream.videoTracks.size() > 0) {
+                    stream.videoTracks.get(0).addSink(remoteRender);
+                }
+                if (videoEnable) {
+                    stream.videoTracks.get(0).setEnabled(true);
+
+                    setSwappedFeeds(false);
+                }
+            }
+
+            @Override
+            public void onClose() {
+
+            }
+        });
+
+    }
+
+    private void setSwappedFeeds(boolean isSwappedFeeds) {
+        this.isSwappedFeeds = isSwappedFeeds;
+        localRender.setTarget(isSwappedFeeds ? remote_view : local_view);
+        remoteRender.setTarget(isSwappedFeeds ? local_view : remote_view);
+    }
+
+    @Override
+    protected void onBindListener() {
+
+    }
+
+    @Override
+    public void show(int marginRight, int marginBottom, OnShowListener onShowListener) {
+        super.show(marginRight, marginBottom, onShowListener);
+    }
+
+    @Override
+    public void showTopRight(OnShowListener onShowListener, Intent captureIntent) {
+        super.showTopRight(onShowListener);
+        Log.e("zrzr", "showTopRight");
+        manager.sendPreCall(mContext, rootEglBase, captureIntent);
+    }
+
+}
---
+++ app/src/main/java/com/example/webrtcandroid/view/FloatingWindowManager.java
@@ -0,0 +1,86 @@
+package com.example.webrtcandroid.view;
+
+import android.content.Context;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * * Copyright * 圣通电力
+ *
+ * @ProjectName: WebrtcAndroid
+ * @Package: com.example.webrtcandroid.view
+ * @ClassName: FloatingWindowManager
+ * @Description: 悬浮窗 工具类
+ * @Author: Raphrodite
+ * @CreateDate: 2023/4/6
+ */
+public class FloatingWindowManager {
+
+    private WindowManager mWindowManager;
+    private static FloatingWindowManager mInstance;
+    private Context mContext;
+
+    public static FloatingWindowManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new FloatingWindowManager(context.getApplicationContext());
+        }
+        return mInstance;
+    }
+
+    private FloatingWindowManager(Context context) {
+        mContext = context;
+        //获得WindowManager对象
+        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+    }
+
+    /**
+     * 添加悬浮窗
+     *
+     * @param view
+     * @param params
+     * @return
+     */
+    public boolean addView(View view, WindowManager.LayoutParams params) {
+        try {
+            mWindowManager.addView(view, params);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 移除悬浮窗
+     *
+     * @param view
+     * @return
+     */
+    public boolean removeView(View view) {
+        try {
+            mWindowManager.removeView(view);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 更新悬浮窗参数
+     *
+     * @param view
+     * @param params
+     * @return
+     */
+    public boolean updateView(View view, WindowManager.LayoutParams params) {
+        try {
+            mWindowManager.updateViewLayout(view, params);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
